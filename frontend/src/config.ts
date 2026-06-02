5fcc40ccec30b817478dc439eb5135a5a3c24a0e
Ryan Zernach <ryan@zernach.com>
2026-06-01 18:30:52 -0600

    cloudflare proxy

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -10,7 +10,7 @@ export const ENV: "local" | "dev" = "dev";
 
 const BACKEND_URLS = {
   local: "http://localhost:8000",
-  dev: "https://api.lyzr.archlife.org",
+  dev: "",
 } as const;
 
 export const BACKEND_URL: string = BACKEND_URLS[ENV];
